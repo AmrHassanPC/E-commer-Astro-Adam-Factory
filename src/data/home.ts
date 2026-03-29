837e5fee7602b64e7c7be5b2d78395acf9d3c4ad
AmrHassanPC <amrhassanpc4@gmail.com>
2026-03-29 16:35:12 +0200

    Changed the site data to Adam Factory, added new pages, and updated the UI to match the new branding. Also updated the package-lock.json and package.json files to reflect the changes in dependencies.

--- src/data/home.ts
+++ src/data/home.ts
@@ -1,50 +1,23 @@
 export const categories = [
     {
-        name: "Computers",
-        image: "https://images.unsplash.com/photo-1547082299-de196ea013d6?q=80&w=2000&auto=format&fit=crop",
-        path: "/category/computers",
+        name: "انتريهات",
+        image: "https://images.unsplash.com/photo-1586023492129-6f87c5baed96?auto=format&fit=crop&w=2000&q=80",
+        path: "/category/انتريهات",
     },
     {
-        name: "Phones",
-        image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?q=80&w=2000&auto=format&fit=crop",
-        path: "/category/phones",
+        name: "ركن",
+        image: "https://images.unsplash.com/photo-1615874959472-497e9a8c45ce?auto=format&fit=crop&w=2000&q=80",
+        path: "/category/ركن",
     },
     {
-        name: "Audio",
-        image: "https://images.unsplash.com/photo-1590658268037-6bf12165a8df?q=80&w=2000&auto=format&fit=crop",
-        path: "/category/audio",
+        name: "صالونات",
+        image: "https://images.unsplash.com/photo-1587392927607-64f1d1ce629a?auto=format&fit=crop&w=2000&q=80",
+        path: "/category/صالونات",
     },
     {
-        name: "Accessories",
-        image: "https://i.ytimg.com/vi/6nF-4i3RrG0/hq720.jpg?sqp=-oaymwEXCK4FEIIDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA1Ir1cXQna8UT0zPxI62l1ZlvvEQ",
-        path: "/category/accessories",
-    },
-];
-
-export const featuredLinks = [
-    {
-        name: "About Us",
-        image:
-            "https://images.unsplash.com/photo-1522071820081-009f0129c71c?auto=format&fit=crop&w=800&q=80",
-        path: "/about",
-        className: "lg:col-span-2",
-        imageClass: "aspect-square lg:aspect-[2/1]",
-    },
-    {
-        name: "Our Blog",
-        image:
-            "https://img.freepik.com/foto-gratis/mensaje-online-blog-chat-comunicacion-envolver-concepto-icono-grafico_53876-139717.jpg?semt=ais_hybrid&w=740&q=80",
-        path: "/blog",
-        className: "lg:col-span-2",
-        imageClass: "aspect-square lg:aspect-[2/1]",
-    },
-    {
-        name: "Contact Us",
-        image:
-            "https://voca-land.sgp1.cdn.digitaloceanspaces.com/-1/1635919600965/contact-us.jpeg",
-        path: "/contact",
-        className: "lg:col-span-4",
-        imageClass: "aspect-[2/1] lg:aspect-[4/1]",
+        name: "ترابيزات",
+        image: "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?auto=format&fit=crop&w=2000&q=80",
+        path: "/category/ترابيزات",
     },
 ];
 
